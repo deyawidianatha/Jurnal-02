33848111307076345289a0e3ab0be16d40482b7f
unknown <widanathadeya@student.telkomuniversity.ac.id>
2020-09-15 23:11:24 -0700

    HelloWorld.java

---
+++ Jurnal2deya/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+	    System.out.println("Nama: Ni Luh Putu Deya Widianatha");
+		System.out.println("NIM: 6706200028");
+		System.out.println("Kelas: D3IF-44-02");
+	}
+}
